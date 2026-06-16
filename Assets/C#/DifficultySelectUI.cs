1fce6f853e1663f5e9b9728dc3a8103cf34ec32b
LSXFMX <A190368808@outlook.com>
2026-06-16 16:07:33 +0800

    revert: 撤销DifficultySelectUI动态查找/克隆按钮的修改，恢复原状

--- Assets/C#/DifficultySelectUI.cs
+++ Assets/C#/DifficultySelectUI.cs
@@ -82,9 +82,8 @@ public class DifficultySelectUI : MonoBehaviour
         if (DifficultyManager.Instance == null) return;
 
         int totalDifficulties = DifficultyManager.Instance.configs.Length;
-        int buttonCount = Mathf.Min(difficultyButtons.Length, totalDifficulties);
 
-        for (int i = 0; i < buttonCount; i++)
+        for (int i = 0; i < difficultyButtons.Length; i++)
         {
             if (difficultyButtons[i] == null) continue;
             int idx = i;
